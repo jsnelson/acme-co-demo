8bf32e5199ba7cc555ed40f34e134fec9f63193b
John Nelson <john@abstracttech.com>
2021-10-11 13:24:05 -0400

    fix errors and add functions

--- acme-api/schema.sql
+++ acme-api/schema.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION pgcrypto;
+
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username TEXT,
@@ -18,8 +20,37 @@ VALUES
 ('Working from lab');
 
 CREATE TABLE user_status (
-    user_id INTEGER REFERENCES(users.id),
-    status_id INTEGER REFERENCES(status.id),
+    user_id INTEGER REFERENCES users(id),
+    status_id INTEGER REFERENCES status(id),
     created TIMESTAMP DEFAULT now(),
     PRIMARY KEY (user_id, status_id, created)
 );
+
+CREATE OR REPLACE FUNCTION make_password(pw TEXT) RETURNS TEXT
+AS $$
+BEGIN
+    RETURN crypt(pw, gen_salt('bf'));
+END;
+$$
+LANGUAGE plpgsql
+COST 10;
+
+CREATE OR REPLACE FUNCTION check_password(user_id INTEGER, pw TEXT) RETURNS BOOLEAN
+AS $$
+DECLARE
+    current_pw TEXT;
+    new_pw TEXT;
+BEGIN
+    SELECT password
+    FROM users
+    WHERE users.id = user_id
+    INTO current_pw;
+
+    SELECT crypt(pw, gen_salt('bf')) INTO new_pw;
+
+    RETURN current_pw = new_pw;
+END;
+$$
+LANGUAGE plpgsql
+COST 50;
+
